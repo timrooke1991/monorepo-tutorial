77176df01068f97705aeda02e650e0b71a311ca3
Tim Rooke <timrooke1991@gmail.com>
2021-05-18 08:53:14 +0100

    fix: project changes
    Signed-off-by: Tim Rooke <timrooke1991@gmail.com>

--- packages/title-to-frontmatter/index.js
+++ packages/title-to-frontmatter/index.js
@@ -13,7 +13,7 @@ module.exports = async (
       .split(process.cwd())
       .pop()
       .replace(/^\//, '')
-    // Skip node if not included - is this required?
+    // Skip node if not included - is this required? er
     if (!mm.isMatch(filePath, include)) { return }
   }
   if(!markdownNode.frontmatter.noTitleToFrontmatter){
